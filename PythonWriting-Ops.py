af35b830473bcf4c66b9fccf9dd47acdde18874e
Aayush3014 <0201csai036@niet.co.in>
2022-07-05 17:16:36 +0530

    commit

--- PythonWriting-Ops.py
+++ PythonWriting-Ops.py
@@ -1,4 +1,22 @@
-f = open("filewrite2.txt","w")
-f.write("Ayush is a good boy\n")
-f.write("Ayush is a bad boy")
+# file writing
+
+# f = open("filewrite2.txt","w")
+# a = f.write("Ayush is a good boy Ayush is a bad boy\n") # storing the number of characters written in the file.
+# # f.write("Ayush is a bad boy\n")
+# print(a)
+# f.close()
+
+# Appending in a file
+
+# f1 = open("filewrite3.txt","a")
+# f1.write("Ayush is a good boy\n")
+# f1.write("Ayush is a bad boy")
+# f1.close()
+
+# file writing and reading at same time
+
+f = open("filewrite2.txt","r+")
+
+print(f.readlines())
+f.write("you")
 f.close()
