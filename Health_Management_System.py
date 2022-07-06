b89368b3718506ef26bb780466e3a8de550c1fa1
Aayush3014 <0201csai036@niet.co.in>
2022-07-06 20:18:38 +0530

    Initial commit

---
+++ Health_Management_System.py
@@ -0,0 +1,103 @@
+# Health management system
+import datetime as d
+
+def getdate():
+    return d.datetime.now()
+
+
+def entry(k):
+    if k == 1:   # input for harry
+        c = int(input("Enter the Choice\n1.Food\n2.Excercise"))
+        if c == 1:
+            value = input("Enter Food\n")
+            with open("HarryFood.txt","a") as har:
+                har.write(str([str(getdate())])+": "+value+"\n")
+      
+      
+        elif c == 2:
+            value = input("Enter Exercise:\n")
+            with open("HarryExercise.txt","a") as har:
+                har.write(str([str(getdate())])+": "+value+"\n")
+    
+
+    if k == 2:   # input for rohan
+        c = int(input("Enter the Choice\n1.Foodrohan\n2.Excerciserohan"))
+        if c == 1:
+            value = input("Enter Food\n")
+            with open("rohanFood.txt","a") as har:
+                har.write(str([str(getdate())])+": "+value+"\n")
+      
+      
+        elif c == 2:
+            value = input("Enter Exercise:\n")
+            with open("rohanExercise.txt","a") as har:
+                har.write(str([str(getdate())])+": "+value+"\n")
+
+
+    if k == 3:   # input for hammad
+        c = int(input("Enter the Choice\n1.Food\n2.Excercise"))
+        if c == 1:
+            value = input("Enter Food\n")
+            with open("HammadFood.txt","a") as har:
+                har.write(str([str(getdate())])+": "+value+"\n")
+      
+      
+        elif c == 2:
+            value = input("Enter Exercise:\n")
+            with open("HammadExercise.txt","a") as har:
+                har.write(str([str(getdate())])+": "+value+"\n")
+
+
+def retrieve(k):
+    if k == 1:   # input for harry
+        c = int(input("Enter the Choice\n1.Food\n2.Excercise"))
+        if c == 1:
+            with open("HarryFood.txt") as har:
+                e = har.readlines()
+                for i in e:
+                    print(i,end=",")
+      
+      
+        elif c == 2:
+            with open("HarryExercise.txt") as har:
+                e = har.readlines()
+                for i in e:
+                    print(i,end=",")
+
+    if k == 2:   # input for rohan
+        c = int(input("Enter the Choice\n1.Food\n2.Excercise"))
+        if c == 1:
+            with open("rohanFood.txt") as har:
+                e = har.readlines()
+                for i in e:
+                    print(i,end=",")
+      
+      
+        elif c == 2:
+            with open("rohanExercise.txt","a") as har:
+                e = har.readlines()
+                for i in e:
+                    print(i,end=",")
+
+
+    if k == 3:   # input for hammad
+        c = int(input("Enter the Choice\n1.Food\n2.Excercise"))
+        if c == 1:
+            with open("HammadFood.txt","a") as har:
+                e = har.readlines()
+                for i in e:
+                    print(i)
+      
+      
+        elif c == 2:
+            with open("HammadExercise.txt","a") as har:
+                e = har.readlines()
+                for i in e:
+                    print(i)
+
+print("Enter Person Number To Enter data : \n1.Harry\n2.Rohan\n3.Hammad")
+a = int(input("Enter number : "))
+if a == 1:
+    entry(a)
+elif a==2:
+    retrieve(a)
