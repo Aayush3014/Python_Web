2a10e23f353f5a141733b20e35d418efc384decc
Aayush3014 <0201csai036@niet.co.in>
2022-07-06 20:40:17 +0530

    commit

--- with_block.py
+++ with_block.py
@@ -2,7 +2,7 @@ with open("filewrite2.txt") as f:
     a = f.readlines()
     print(a)
 
-f = open("filewrite2.txt","r+")
+f = open("filewrite2.txt","r+")    #  "r+"" is for reading and writing.
 
 print(f.readlines())
 # f.write("you")
