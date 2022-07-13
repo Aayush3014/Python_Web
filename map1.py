ad49ea370345092ee7f5daaec4cd4dcd07418242
Aayush3014 <0201csai036@niet.co.in>
2022-07-13 17:30:55 +0530

    commit

---
+++ map1.py
@@ -0,0 +1,26 @@
+# l = ["9","7","6","3"]
+# a = list(map(int,l))
+# print(a)
+
+# def sq(a):
+#     return a*a
+
+# num = [4,5,7,8,9,6,2]
+
+
+# square = list(map(sq,num))
+# print(square)
+
+
+# square = list(map(lambda x: x*x,num))
+# print(square)
+
+def square(a):
+    return a*a
+def cube(a):
+    return a*a*a
+func = [square,cube]
+# num = [4,5,7,8,9,6,2]
+for i in range(5):
+    val = list(map(lambda x:x(i),func))
+    print(val)
