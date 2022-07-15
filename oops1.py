501656d6ea6c61f6bcef7d81ad05a2fd633b623a
Aayush3014 <0201csai036@niet.co.in>
2022-07-15 20:53:16 +0530

    commit

---
+++ oops1.py
@@ -0,0 +1,10 @@
+class Student:
+    pass
+harry = Student()
+larry = Student()
+harry.name = "jhsghj"
+harry.Class = 5
+larry.name = "j555ghj"
+larry.Class = 9
+print(harry.name,harry.Class)
+print(larry.name,larry.Class)
