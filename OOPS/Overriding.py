24a01a43c5d5df62fa6cb9668eaeb3efa1951a83
Aayush3014 <0201csai036@niet.co.in>
2022-07-22 20:25:59 +0530

    commit

---
+++ OOPS/Overriding.py
@@ -0,0 +1,18 @@
+from tkinter import ON
+
+
+class One:
+    classvar1 = "I am a class variable of Class one."
+    def __init__(self) -> None:
+        self.var1 = "variable 1 of class One constructor."
+        self.classvar1 = "Instance variable of class One"
+    
+class Two(One):
+    classvar2 = "I am a class variable of Class two."
+    def __init__(self) -> None:
+        super().__init__()
+        self.var2 = "variable 2 of class two constructor."
+
+a = One()
+b = Two()
+print(b.classvar1)
