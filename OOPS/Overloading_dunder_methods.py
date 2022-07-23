14a452d2aa16ae3cce6aa77fb49146f854385a19
Aayush3014 <0201csai036@niet.co.in>
2022-07-23 16:37:40 +0530

    commit

--- OOPS/Overloading_dunder_methods.py
+++ OOPS/Overloading_dunder_methods.py
@@ -1,3 +1,8 @@
+# https://docs.python.org/3/library/operator.html
+# link of funtions for overloading methods using dunder methods.
+
+
+
 class Employee:
     no_of_leaves = 10
 
@@ -22,10 +27,19 @@ class Employee:
 
     def __truediv__(self, other):
         return self.salary / other.salary
+
+
+# Print function will give priority to str function if both function are provided
+# If only one is provided the calling str and repr will return only which function is overloaded.
+# As given in below code.
+
     def __repr__(self):
         return f"Employee('{self.name}',{self.salary},'{self.role}')"
+
     def __str__(self) -> str:
         return f"name is {self.name}. Salary is {self.salary}.role is {self.role}"
+
+
 harry = Employee("Harry", 45, "t")
 # rohan = Employee("Rohan", 5, "s")
 
