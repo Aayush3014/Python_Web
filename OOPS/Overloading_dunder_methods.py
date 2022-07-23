fa4366896b5f0642f97ecb7f5aabf5b9b8f69945
Aayush3014 <0201csai036@niet.co.in>
2022-07-23 16:34:58 +0530

    commit

---
+++ OOPS/Overloading_dunder_methods.py
@@ -0,0 +1,34 @@
+class Employee:
+    no_of_leaves = 10
+
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):   # Instance Methods
+        return f"name is {self.name}. Salary is {self.salary}.role is {self.role}"
+
+    @classmethod
+    def info_leaves(cls):
+        return cls.no_of_leaves
+
+    @staticmethod
+    def info():
+        print("hellooooooooo")
+
+    def __add__(self, other):                # Special dunder method created to overload add operator
+        return self.salary + other.salary  # salary of both objects.
+
+    def __truediv__(self, other):
+        return self.salary / other.salary
+    def __repr__(self):
+        return f"Employee('{self.name}',{self.salary},'{self.role}')"
+    def __str__(self) -> str:
+        return f"name is {self.name}. Salary is {self.salary}.role is {self.role}"
+harry = Employee("Harry", 45, "t")
+# rohan = Employee("Rohan", 5, "s")
+
+# print(harry + rohan)
+# print(harry/rohan)
+print(harry)
