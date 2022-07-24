3bd327896988cb0a4849d999593105ebc34e78e5
Aayush3014 <0201csai036@niet.co.in>
2022-07-24 22:50:26 +0530

    commit

---
+++ OOPS/@prop_deco.py
@@ -0,0 +1,17 @@
+class Employee:
+    def __init__(self, fname, lname) -> None:
+        self.fname = fname
+        self.lname = lname
+        # self.email = unge
+
+    def printdetails(self):
+        return f"The Employee is {self.fname} {self.lname}"
+    @property
+    def email1(self):
+        return f"{self.fname}.{self.lname}@google.com"
+
+ayush = Employee("Ayush","Kumar")
+# print(ayush.printdetails())
+print(ayush.email1)
+ayush.fname = "Us"
+print(ayush.email1)
